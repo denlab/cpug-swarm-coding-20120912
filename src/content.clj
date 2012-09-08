4ef7bddbfa76dba3f9649ed07a8c9ec4af1d5893
Denis Labaye <denis.labaye@gmail.com>
2012-09-08 22:28:42 +0200

    final: invitation to the swarm

---
+++ src/content.clj
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+{:cpug-swarm-coding-20120912
+ [{:what         "Swarm coding for the Clojure Paris User Group"}
+  {:when         "20120912-19h00"}
+  {:where
+   "Mac Bride Irish Pub
+    54 Rue Saint-Denis
+    75001 Paris, France"}
+  {:register     "http://goo.gl/Aj74X"}
+  {:requirements [{:hardware
+                   [{:laptop         "Any laptop, the smaller the better!"}
+                    {:laptop-battery "Please charge it ;-)"}]}
+                  {:software         "A SSH client, that's all"}
+                  {:money            "Bring some money to buy drinks (it's the happy hour)"}]}
+  {:faq
+   [{:more-info?               {:ask-clojure-fr-mailing-list
+                                "http://goo.gl/vtszT"}}
+    {:what-is-swarm-coding?    ["It's like pair-programming but with more than 2"
+                                "See: http://goo.gl/PDKtz"]}
+    {:what-editor-will-we-use? ["A command line based one"
+                                "Probably Emacs"
+                                "Maybe Vi if there is someone proficient enough"]}]}]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
